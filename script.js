3f950ed7969e8743a521aaf6583e500520f43e4d
Anthony Boyle <anthonyboyle6@gmail.com>
2023-07-19 02:36:56 +0100

    js code added for color picker

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('js is loaded')
+
+
+// Colors (variables for color selections)
+const mainColorPicker = document.querySelector('#color');
+const backgroundColorPicker = document.querySelector('#bg-color');
+
+// update the label eg #000000
+const mainColorValue = document.querySelector('#color-value');
+const backgroundColorValue = document.querySelector('#bg-color-value');
+
+// add user selected color to value variable
+const updateColor = e => {
+    // e.target.value takes in value ***
+    // stored in value which becomes display text
+    const value = e.target.value;
+    console.log(value);
+    // 
+    mainColorValue.innerText = value;
+}
+const updateBackgroundColor = e => {
+    const value = e.target.value;
+    console.log(value);
+    backgroundColorValue.innerText = value;
+}
+
+// when user selects a color from the picker it will be sent to the API
+// also, we want the eg; id="color-value">#000000 to be updated
+// Add Event Listener Function
+const addColorPickerEventListeners = () => {
+    // get the main color picker (above), 
+    // add an event listener to it that is going to be called 
+    //whenever there is a change and then call function
+    // updateColor
+mainColorPicker.addEventListener('change',updateColor);
+backgroundColorPicker.addEventListener('change',updateBackgroundColor);
+};
+
+
+addColorPickerEventListeners();
+
+//Sliders
